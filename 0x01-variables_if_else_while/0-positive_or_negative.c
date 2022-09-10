57cd2f2182760ca7c6a4851a3cdc07ef3a3e5f58
IMGN-DRGN <thefelainme@gmail.com>
2022-09-09 20:54:26 -0700

    positive commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,12 @@
 /**
  *
  * Return: Always (Success)
- *  */
+ */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -24,6 +26,7 @@ int main(void)
 	{
 	   printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
 
