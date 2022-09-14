1f220972bc42b1f4bf92995fd4738abb43664cd8
IMGN-DRGN <thefelainme@gmail.com>
2022-09-13 23:38:15 -0700

    5 signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_sign - function to check for a sign of a number
+ * @n: is the int that will use for the argument of the function
+ * Return: 0
+ */
 int print_sign(int n)
 {
 if (n > 0)
@@ -14,7 +18,7 @@ return(-1);
 }
 else
 {
-_putchar('0')
+_putchar('0');
 return(0);
 }
 }
