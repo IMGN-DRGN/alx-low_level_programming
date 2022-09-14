c89f695fe429f2367ab514a0f92c12df69ac7a50
IMGN-DRGN <thefelainme@gmail.com>
2022-09-14 09:16:42 -0700

    not gonna give up dawg

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,6 +9,7 @@ void jack_bauer(void)
 	  int hours = 0;
 	  int minutes = 0;
 	  int hours_remainder;
+	  int mins_remainder;
 
 	  while (hours <= 23)
 {
@@ -26,4 +27,7 @@ void jack_bauer(void)
 		minutes++;
 		_putchar('\n');
 	}
+	        hours++
+		minutes = 0;
+}
 }
