74aaecbd668b162526d5c922daa573f29c1e7e8d
IMGN-DRGN <thefelainme@gmail.com>
2022-09-20 00:32:56 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,18 @@
 
 void rev_string(char *s)
 {
-char rev = s[0];
-int fcounter = 0;
-int i;
+	        int ini = 0, finish = 0; char t;
 
-while (s[fcounter] != '\0')
-fcounter++;
+        while (s[finish] != 0)
+	                finish++;
+        finish--;
 
-for (i = 0; i < fcounter; i++)
+        while (ini < finish)
 {
-fcounter--;
-rev = s[i];
-s[i] = s[fcounter];
-s[fcounter] = rev;
+                t = s[ini];
+                s[ini] = s[finish];
+                s[finish] = t;
+                ini++;
+                finish--;
 }
 }
