3c5a1e091c8dff644ac1c10d535c45d8040a3d38
IMGN-DRGN <thefelainme@gmail.com>
2022-09-22 18:31:13 -0700

    fresh commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,19 +14,20 @@
 void reverse_array(int *a, int n)
 
 {
-int *p, i, aux, k;
+	int *p, i, aux, k;
 
-p = a;
+	p = a;
 
-for (i = 1; i < n; i++)
-{
-p++
-}
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
 
-for (k = 0; k < i / 2; k++)
-{
-aux = a[k];
-a[k] = *p;
-*p = aux;
-}
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
 }
