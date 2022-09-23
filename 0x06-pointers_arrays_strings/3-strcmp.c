ec57a4c2feac68fa9bfe90bcbdcb8d9a19bcdc80
IMGN-DRGN <thefelainme@gmail.com>
2022-09-22 17:33:55 -0700

    3rd in row

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,56 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
 /**
- *_strcmp - compares two strings
- *@s1: A pointer to a character that will be changed
- *@s2: A pointer to a character that will also be changed/modified/updated
- *Return: dest
- */
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
 
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 
 {
+int a = 0, b = 0, c = 0, r = 0, lim;
 
-char *str_one = s1;
-char *str_two = s2;
-
-
-while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+while (s1[a])
 {
-str_one++;
-str_two++;
+a++;
 }
 
-return (*str_one - *str_two);
+while (s2[b])
+{
+b++;
+}
+
+if (a <= b)
+{
+lim = a;
+}
+
+else
+{
+lim = b;
+}
+
+while (c <= lim)
+{
+if (s1[c] == s2[c])
+{
+c++;
+continue;
+}
+else
+{
+r = s1[c] - s2[c];
+break;
+}
+
+c++;
+}
+
+return (r);
 }
